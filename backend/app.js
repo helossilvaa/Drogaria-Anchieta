c473840f344c307d51ef66fbb3e1561764783b9c
viormelli <ormellivitoria@gmail.com>
2025-10-15 16:46:20 -0300

    caixa, pagamentos etc

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,6 @@ import express from 'express';
 import cors from 'cors';
 import session from 'express-session';
 import dotenv from 'dotenv';
-
 import usuarioRotas from './routes/usuarioRotas.js';
 import vendasRotas from './routes/vendasRotas.js';
 import tipoPagamentoRotas from './routes/tipoPagamentoRotas.js';
