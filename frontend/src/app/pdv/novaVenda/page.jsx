ff602eabbdceb4d554a14df1de79d678a1062712
viormelli <ormellivitoria@gmail.com>
2025-10-17 16:36:02 -0300

    PVD back e front

---
+++ frontend/src/app/pdv/novaVenda/page.jsx
@@ -0,0 +1,12 @@
+import Contact02Page  from "@/components/contact-02/contact-02";
+
+export default function Zenith(){
+
+    return(
+        <>
+       <Contact02Page>
+        
+       </Contact02Page>
+        </>
+    )
+}
